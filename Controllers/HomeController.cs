a16013ed18c13ac33a21ef53d95fa68c8ca0f5f0
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2022-11-18 15:00:38 -0500

    Updated Home Controller FizzBuzz Post

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,9 +13,52 @@ namespace FizzBuzz_MVC.Controllers
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult FizzBuzzPage()
         {
-            return View();
+            FizzBuzz model = new FizzBuzz();
+
+            model.FizzValue = 3;
+            model.BuzzValue = 5;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FizzBuzzPage(FizzBuzz model)
+        {
+            List<string> fbItems = new List<string>();
+
+            bool Fizz;
+            bool Buzz;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Fizz = (i % model.FizzValue == 0);
+                Buzz = (i % model.BuzzValue == 0);
+
+                if (Fizz == true && Buzz == true)
+                {
+                    fbItems.Add("FizzBuzz");
+                }
+                else if (Fizz == true)
+                {
+                    fbItems.Add("Fizz");
+                }
+                else if (Buzz == true)
+                {
+                    fbItems.Add("Buzz");
+                }
+                else
+                {
+                    fbItems.Add(i.ToString());
+                }
+            }
+
+            model.Results = fbItems;
+
+            return View(model);
         }
 
         public IActionResult Index()
